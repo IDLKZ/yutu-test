2445aa5175fc189493fb3335ca02f634212ea54e
Misti <mistier.famous@gmail.com>
2022-04-05 22:31:56 +0600

    123

--- lib/app/helpers/api_constants.dart
+++ lib/app/helpers/api_constants.dart
@@ -6,9 +6,10 @@ abstract class ApiConstants{
   static String FCM_AUTH = "AAAAfSXUaf4:APA91bEtlnK0bwphVAtrtuYfHCiaBpjbqM-A2Yb2e5T_St9oM-JubRUEBlaTzdLYiSeA8ZjurJTSxzwMU0T3LYNZamctGMWGFbcsc6pYDcuWH46TUsUBJKSgnww6AZcpPG5FhOMmFG0e";
 
 
-  static String RapidApiKey = "25a8becb95msh3eb6ceb28022821p14dd94jsnb46668275791";
+  //static String RapidApiKey = "25a8becb95msh3eb6ceb28022821p14dd94jsnb46668275791";
   static String RapidEndPoint = "wft-geo-db.p.rapidapi.com";
   static String RapidEndPointCity = "/v1/geo/cities";
+  static String RapidApiKey  = "3f246dc9b3msh4f497a2d1c19437p1f07e9jsn5d5ae651b5d7";
 
 
 
